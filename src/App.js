b7f5daae31aff1255b1fae511c781e51b6f15bd8
Anya <boanya5@mail.ru>
2021-08-10 21:33:58 +0300

    HW3 Map tasks to view

--- src/App.js
+++ src/App.js
@@ -1,25 +1,69 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react';
+import './styles.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Hello Valera!
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+const Task = ({ id, name, description, completed }) => (
+  <div className="task-parent">
+    <h3>{name}</h3>
+    <div>{description}</div>
+    {completed ? <div>Completed</div> : <div>Not Completed</div>} 
+    <button onClick={() => handleClick({id: id, completed: completed})}>Complete</button>
+  </div>
+)
+
+const handleClick = ({id, completed}) => {
+  console.log('Task ' + id + ' completed status = ' + completed)
+}
+
+class TodoList extends React.Component{
+  state = {
+    tasks: [
+      {
+        "id": 1,
+        "name": "Wash the curtains",
+        "description": 'living room + bedroom curtains',
+        "completed": true
+      },
+      {
+        "id": 2,
+        "name": "Wash the dishes",
+        "description": 'kitchen',
+        "completed": false
+      },
+      {
+        "id": 3,
+        "name": "Buy new pillows",
+        "description": 'ikea',
+        "completed": false
+      },
+      {
+        "id": 4,
+        "name": "HW3 React",
+        "description": 'Hi Valera',
+        "completed": true
+      }
+    ]
+  }
+
+  TaskList = (tasks) => {
+    return (
+      <div>
+        {tasks.map(it => <Task key={it.id} id={it.id} name={it.name} description={it.description} completed={it.completed} />)}
+      </div>
+    )
+  }
+
+  render () {
+    return (
+      <div className="parent-task-tracker">
+        <h1 className="header">Task Tracker</h1>
+        <div>{this.TaskList(this.state.tasks)}</div>
+      </div>
+    )
+  }
+}
+
+const App = () => {
+    return <TodoList />
 }
 
 export default App;
