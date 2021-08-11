92efab847deb975d54460470e6d6def9b3bed7b9
Anya <boanya5@mail.ru>
2021-08-12 01:07:56 +0300

    TaskList extracted into separate component

--- src/App.js
+++ src/App.js
@@ -5,7 +5,7 @@ const Task = ({ id, name, description, completed }) => (
   <div className="task-parent">
     <h3>{name}</h3>
     <div>{description}</div>
-    {<div>{completed ? "Completed" : "Not Completed"}</div>} 
+    <div>{completed ? "Completed" : "Not Completed"}</div>
     <button onClick={() => handleClick({id: id, completed: completed})}>Complete</button>
   </div>
 )
@@ -14,7 +14,19 @@ const handleClick = ({id, completed}) => {
   console.log('Task ' + id + ' completed status = ' + completed)
 }
 
-class TodoList extends React.Component{
+class TodoList extends React.Component {
+
+  render () {
+    return (
+      <div className="parent-task-tracker">
+        <h1 className="header">Task Tracker</h1>
+        <TaskList />
+      </div>
+    )
+  }
+}
+
+class TaskList extends React.Component {
   state = {
     tasks: [
       {
@@ -46,20 +58,11 @@ class TodoList extends React.Component{
 
   render () {
     return (
-      <div className="parent-task-tracker">
-        <h1 className="header">Task Tracker</h1>
-        <div>{TaskList(this.state.tasks)}</div>
+      <div>
+        {this.state.tasks.map(it => <Task key={it.id} id={it.id} name={it.name} description={it.description} completed={it.completed} />)}
       </div>
     )
   }
-}
-
-const TaskList = (tasks) => {
-  return (
-    <div>
-      {tasks.map(it => <Task key={it.id} id={it.id} name={it.name} description={it.description} completed={it.completed} />)}
-    </div>
-  )
 }
 
 const App = () => {
